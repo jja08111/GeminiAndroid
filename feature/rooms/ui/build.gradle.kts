dec0d4b280d33a1470b93b2c537023f55b6629d5
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2024-02-18 00:39:04 +0900

    Rename module to :feature:rooms

---
+++ feature/rooms/ui/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+  id("jja08111.android.library")
+  alias(libs.plugins.ktlint)
+}
+
+android {
+  namespace = "io.github.jja08111.feature.rooms.ui"
+}
+
+dependencies {
+
+  implementation(libs.androidx.core.ktx)
+  implementation(libs.androidx.appcompat)
+  implementation(libs.material)
+  testImplementation(libs.junit)
+  androidTestImplementation(libs.androidx.test.ext.junit)
+  androidTestImplementation(libs.espresso.core)
+}
