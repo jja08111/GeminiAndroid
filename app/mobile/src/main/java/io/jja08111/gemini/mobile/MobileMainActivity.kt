6532cce9d17fc6c630faa68b4e13b5421236a378
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2024-02-18 13:12:43 +0900

    Change ktlint rule

--- app/mobile/src/main/java/io/jja08111/gemini/mobile/MobileMainActivity.kt
+++ app/mobile/src/main/java/io/jja08111/gemini/mobile/MobileMainActivity.kt
@@ -33,10 +33,7 @@ class MobileMainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(
-  name: String,
-  modifier: Modifier = Modifier,
-) {
+fun Greeting(name: String, modifier: Modifier = Modifier) {
   Text(
     text = "Hello $name!",
     modifier = modifier,
