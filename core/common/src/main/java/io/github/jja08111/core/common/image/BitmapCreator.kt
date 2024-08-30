24c17ecbbcfaa1d0c560060773db65cc85e9719f
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2024-08-30 23:29:23 +0900

    Change to use Dispatchers.IO in BitmapCreator

--- core/common/src/main/java/io/github/jja08111/core/common/image/BitmapCreator.kt
+++ core/common/src/main/java/io/github/jja08111/core/common/image/BitmapCreator.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -14,12 +16,13 @@ import javax.inject.Singleton
 class BitmapCreator @Inject constructor(
   @ApplicationContext private val context: Context,
 ) {
-  fun create(imageUri: Uri): Bitmap {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-      ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, imageUri))
-    } else {
-      @Suppress("DEPRECATION")
-      MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+  suspend fun create(imageUri: Uri): Bitmap =
+    withContext(Dispatchers.IO) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, imageUri))
+      } else {
+        @Suppress("DEPRECATION")
+        MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+      }
     }
-  }
 }
