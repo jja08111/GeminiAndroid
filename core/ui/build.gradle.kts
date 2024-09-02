0b583d541dbf6ac9cacbde31825a5a5d57a4f4ec
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2024-09-03 01:03:26 +0900

    Implement CrossFadeTest

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -5,10 +5,17 @@ plugins {
 
 android {
   namespace = "io.jja08111.gemini.core.ui"
+
+  defaultConfig {
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 }
 
 dependencies {
 
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.animation.android)
+  androidTestImplementation(libs.androidx.ui.test.junit4.android)
+  androidTestImplementation(libs.androidx.compose.material3)
+  androidTestImplementation(libs.espresso.core)
 }
---
+++ core/ui/src/androidTest/java/io/jja08111/gemini/core/ui/CrossFadeTest.kt
@@ -0,0 +1,55 @@
+package io.jja08111.gemini.core.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import org.junit.Rule
+import org.junit.Test
+
+class CrossFadeTest {
+  @get:Rule
+  val composeRule = createComposeRule()
+
+  @Test
+  fun shouldSwitchToSecondComponent() {
+    var showFirst by mutableStateOf(true)
+    composeRule.setContent {
+      CrossFade(
+        showFirst = showFirst,
+        firstContent = { FirstComponent() },
+        secondContent = { SecondComponent() },
+      )
+    }
+
+    composeRule.onNodeWithText(FIRST_TITLE).assertIsDisplayed()
+    composeRule.onNodeWithText(SECOND_TITLE).assertIsNotDisplayed()
+
+    showFirst = false
+
+    composeRule.onNodeWithText(FIRST_TITLE).assertIsNotDisplayed()
+    composeRule.onNodeWithText(SECOND_TITLE).assertIsDisplayed()
+  }
+
+  @Suppress("TestFunctionName")
+  @Composable
+  private fun FirstComponent() {
+    Text(text = FIRST_TITLE)
+  }
+
+  @Suppress("TestFunctionName")
+  @Composable
+  private fun SecondComponent() {
+    Text(text = SECOND_TITLE)
+  }
+
+  companion object {
+    private const val FIRST_TITLE = "First"
+    private const val SECOND_TITLE = "Second"
+  }
+}
